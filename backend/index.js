50e95f4487857d093120d3eac4119c5354f9456b
xynthiawan <xdw201205@gmail.com>
2023-04-29 00:26:24 -0400

    userdemo

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,21 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
+
+    
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try{
+   const r = await axious.put (
+    'https://api.chatengine.io/users/',
+    {username:username, secret:username, first_name:username},
+    {headers:{"private-key": "5f08d5e0-d29a-4fd0-a3e9-c44cb3f918d3 "}}
+   )
+   return res.status (r.status).json(r.data)
+  }catch (e){
+    return res.status(e.response.status).json(e.response.data)
+
+  }
 });
 
 app.listen(3001); 

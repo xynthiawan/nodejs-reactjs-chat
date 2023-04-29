e57969d3a128e6c0bee2db95a5320854ee8890cb
xynthiawan <xdw201205@gmail.com>
2023-04-29 01:27:14 -0400

    user sign up worked

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,16 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
-});
 
+  try{
+    const r = await axios.put(
+      'https://api.chatengine.io/users',
+      { username:username, secret: username, first_name: username},
+      {headers:{"private-key":"f8c7914b-d7a0-4973-8191-9857ce86d84d"}}
+    )
+    return res.status (r.status).json(r.data)
+  }catch (e){
+    return res.status(e.response.status).json(e.response.data)
+  }
+});
 app.listen(3001);

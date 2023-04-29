5bd3e6d1dc8853fa542b470edf14f0c24982afdf
xynthiawan <xdw201205@gmail.com>
2023-04-29 01:11:05 -0400

    went back and fix bug

--- backend/index.js
+++ backend/index.js
@@ -6,21 +6,8 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-
-    
   const { username } = req.body;
-
-  try{
-   const r = await axious.put (
-    'https://api.chatengine.io/users/',
-    {username:username, secret:username, first_name:username},
-    {headers:{"private-key": "f8c7914b-d7a0-4973-8191-9857ce86d84d "}}
-   )
-   return res.status (r.status).json(r.data)
-  }catch (e){
-    return res.status(e.response.status).json(e.response.data)
-
-  }
+  return res.json({ username: username, secret: "sha256..." });
 });
 
-app.listen(3001); 
+app.listen(3001);

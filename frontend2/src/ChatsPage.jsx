08c9a22dcab8b370dfa3d1d6c2322f2dbbbd41e7
xynthiawan <xdw201205@gmail.com>
2023-04-29 02:24:17 -0400

    finished!

--- frontend2/src/ChatsPage.jsx
+++ frontend2/src/ChatsPage.jsx
@@ -1,5 +1,17 @@
-const ChatsPage = ()=>{
-    return <>Chats...</>
+
+import{PrettyChatWindow} from 'react-chat-engine-pretty'
+const ChatsPage = (props)=>{
+    
+    return (
+        <div style={{ height: '100vh'}}>
+       <PrettyChatWindow 
+       projectId='5f08d5e0-d29a-4fd0-a3e9-c44cb3f918d3'
+       username={props.user.username}
+       secret={props.user.secret}
+       style={{height: '100%'}}
+       />
+    </div>
+    )
 }
 
 export default ChatsPage
